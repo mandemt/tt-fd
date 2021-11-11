c61697a7c0fe6ec03bea8bdb64d2486f9dacc86d
mandemt <70643363+mandemt@users.noreply.github.com>
2021-11-11 12:46:55 +0100

    De antwoorden op de vraag 'hoeveel zin heb je in de tech track' staan nu in een bar chart

--- d3-tutorial/p1/index.js
+++ d3-tutorial/p1/index.js
@@ -1,29 +1,87 @@
-const margin = {top: 100, 
-                bottom: 0,
-                left: 100,
-                right: 100}
-
+const margin = {top: 40, bottom: 10, left: 120, right: 20};
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
-const plaatje = d3.select('body').append('svg')
-    .attr('width', width+margin.left + margin.right)
-    .attr('height', height+margin.top + margin.bottom)
+const svg = d3.select('body').append('svg')
+.attr('width', width+margin.left+margin.right)
+.attr('height', height+margin.top+margin.bottom);
 
-    const group = plaatje.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+// Group used to enforce margin
+const g = svg.append('g')
+.attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const data = [10,20,30,40,50,60];
 
-    const bar_height = 100;
+const xscale = d3.scaleLinear().range([0,500]);
+const yscale = d3.scaleBand().range([0, 10]).paddingInner(0.1)
 
-    const vierkant = group.selectAll('rect').data(data).join(
-        (enter) => enter.append('rect').attr('x', 10),
+const xaxis = d3.axisTop().scale(xscale)
+const yaxis = d3.axisLeft().scale(yscale);
+const bar_height = 10;
+const g_xaxis = g.append('g').attr('class', 'x axis');
+const g_yaxis = g.append('g').attr('class', 'y axis');
+
+
+
+const rect = g.selectAll('rect').data(data).join(
+    (enter) => {
+        const rect_enter = enter.append('rect').attr('x', 0)
+        rect_enter.append('title')
+        return rect_enter
+    },
+    (update) => update,
+    (exit) => exit.remove() // als er te veel dom object zijn verwijder ze
+);
+update(data)
+
+function update(new_data) {
+    //update the scales
+    xscale.domain([0, 10]);
+    // yscale.domain([0]);
+    g_xaxis.call(xaxis);
+    g_yaxis.call(yaxis);
+
+
+    // Render the chart with new data
+
+    // DATA JOIN
+    const vierkant = g.selectAll('rect').data(new_data).join(
+        // ENTER 
+        // new elements
+        (enter) => {
+            const rect_enter = enter.append('rect').attr('x', 0);
+            rect_enter.append('title');
+            return rect_enter;
+        },
+        // UPDATE
+        // update existing elements
         (update) => update,
+        // EXIT
+        // elements that aren't associated with data
         (exit) => exit.remove()
     );
-    
-vierkant
-    .attr('height', bar_height)
-    .attr('width', (a) => a * 7)
-    .attr('y', (data, i) => i*(bar_height+5))
+
+    // ENTER + UPDATE
+    // both old and new elements
+
+    rect.select('title').text((d) => d[1]);
+
+    rect.transition()
+        .attr('height', yscale.bandwidth())
+        .attr('width', (data) => xscale(data.zin) )
+        .attr('y', (d,i) => i*(bar_height+5))
+}
+
+
+
+
+// Render the chart with new data
+
+// DATA JOIN
+
+// ENTER
+// new elements
+
+// ENTER + UPDATE
+// both old and new elements
+
+// EXIT
